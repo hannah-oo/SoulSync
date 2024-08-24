17f15cfbe5df8610814a9d21aa811c6987f77b3b
nguyendang <104487618+nguyendang2004@users.noreply.github.com>
2024-08-24 20:02:35 +1000

    Add files via upload
    Add the map. You might need to merge with the current curve

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template, request, abort, url_for
-from flask_socketio import SocketIO
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
+# Example coordinates list
+coordinates = [
+    {'lat': 51.505, 'lon': -0.09, 'number': 1},
+    {'lat': 21.515, 'lon': -0.1, 'number': 2},
+    {'lat': 31.525, 'lon': -0.12, 'number': 7}
+]
+
+# coordinates = [
+#     ([51.505, -0.09], 1),
+#     ([51.515, -0.1],2),
+#     ([51.525, -0.12],6)
+# ]
 @app.route('/')
-def home():
-    return render_template('main.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/coordinates')
+def get_coordinates():
+    return jsonify(coordinates)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(debug=True)
